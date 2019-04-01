1a1b65e572fa90fc86e99945c1e19df0decca313
argromadzki <41020899+argromadzki@users.noreply.github.com>
2019-04-01 17:32:56 -0400

    put logistic into file with binary/multi function

--- Code/logistic_base.py
+++ Code/logistic_base.py
@@ -52,11 +52,70 @@ print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(lo
 
 
 
-confusion_matrix = confusion_matrix(y_test, y_pred)
-print("===Confusion Matrix===\n",confusion_matrix,"\n\n")
+confuse = confusion_matrix(y_test, y_pred)
+print("===Confusion Matrix===\n",confuse,"\n\n")
 
 # throws an error because our multi-class log doesn't predict for each label --> basically just predicts majority class
 # print(classification_report(y_test, y_pred))
+
+
+
+
+
+# write function
+
+
+def baseline_log(df, multi = None):
+    X = df.iloc[:,0:2]
+    y = df.iloc[:,2:3]
+    pd.value_counts(y['target'])
+    n = X.shape[0]
+    cutoff = n-(n//8) # total - the number you want to test, which here i'm flooring 
+    #                   (amount you want in training should be 1/10th value the denominator)
+
+    X_train, X_test = (X.iloc[0:cutoff , :] , X.iloc[cutoff: , :] )
+    # X_train.tail()
+    # X_test.head()
+
+    y_train, y_test = (y.iloc[0:cutoff , :].values.ravel() , y.iloc[cutoff: , :].values.ravel() )
+
+    # X_train.shape[0] == y_train.shape[0]
+
+    if multi == True:
+        logreg = LogisticRegression(multi_class='ovr')
+        # coeff did not converge for sag, newton-cg -- lbfgs also gives all one class
+    else:
+        logreg = LogisticRegression(solver='lbfgs')
+
+    logreg.fit(X_train, y_train)
+
+    y_pred = logreg.predict(X_test)
+    unique, counts = np.unique(y_pred, return_counts=True)
+    print (np.asarray((unique, counts)).T)
+    print('\n\nAccuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+    confuse = confusion_matrix(y_test, y_pred)
+    print("\n\n\n===Confusion Matrix===\n",confuse,"\n\n")
+    print(classification_report(y_test, y_pred))
+
+
+
+
+# get rid of one class
+df2=df[df['target']!=1]
+df2['target'].value_counts()
+
+
+baseline_log(df2)
+
+df2
+
+
+
+
+
+
+
+
 
 # ================================================
 # cannot multi-class
